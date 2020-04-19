f44e539c73b4adb5237d00a2c0c9e3e19cfe458e
keyboradmen <uguess@189.cn>
2020-04-19 22:20:39 +0800

    update

--- 密码生成工具/main.py
+++ 密码生成工具/main.py
@@ -1,13 +1,23 @@
 import random
+import time
 
 length = int(input("Input length: "))
 pwd = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"~!@#$%^&*()-=,.`|'
 
 ls = []
 
-while len(ls) < length:
-    for i in range(length):
-        ls.append(pwd[random.randint(0, len(pwd) - 1)])
+while True:
 
+    while len(ls) < length:
+        for i in range(length):
+            ls.append(pwd[random.randint(0, len(pwd) - 1)])
 
-print("".join(ls))
+    else:
+        print("".join(ls))
+        print("Do you like it ? [yes or no] ")
+        if input() in ("no", "No", 'NO'):
+            ls.clear()
+            continue
+
+        else:
+            break
